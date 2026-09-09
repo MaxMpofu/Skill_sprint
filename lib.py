aba044730e3c2377dc96915faf78209a7c94eb97
mhlabaandile4-del <mhlabaandile4@gmail.com>
2026-09-09 15:14:11 -0700

    added an email attribute in the student and lecturer class on lib.py
    I've imported steamlit too on main.py

--- lib.py
+++ lib.py
@@ -1,18 +1,22 @@
 #STUDENT CLASS
 class Student:
-    def __init__(self, full_name, campus, password, short_bio, repo_link, rank):
+    def __init__(self, full_name, campus, email, password, short_bio, repo_link, rank):
         self.full_name = full_name
         self.campus    = campus
+        self.email     = email
         self.password  = password
         self.short_bio = short_bio
         self.repo_link = repo_link
         self.rank      = rank
+
+    
 #STUDENT CLASS
 #LECTURER CLASS   
 class Lecturer:
-    def __init__(self, full_name, campus, password, short_bio):
+    def __init__(self, full_name, campus, email, password, short_bio):
         self.full_name = full_name
         self.campus    = campus
+        self.email     = email
         self.password  = password
         self.short_bio = short_bio
 #LECTURER CLASS
--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 import lib
+import streamlit as st
 
-#This is where the main program lies
+#I'm planning to create a simple modal that asks for user information and stores it in firebase, add a simple profile view later
+st.title("Welcome to the Streamlit App")
+#JUANDRE will deal with Ai api intergration, just make relaible and fast, add simple a one week timer variable and prompt the API to return a simple "task for the week" and store it in TASK variable 
+#SASHA and MAXWELL will deal with the simple leader board, add a simple leaderboard that shows the top 10 students based on their rank, and make sure to update it in real-time as students complete tasks.
+#This a collaboration project, so anyone is welcome to ask for help.
